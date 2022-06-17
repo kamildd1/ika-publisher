2845f2bdc3eddb7a79b54e67e784fd38a0708bdd
Mateusz <mateusz.zaranek123@gmail.com>
2022-06-17 13:08:12 +0200

    form

--- src/main/java/com/ikea/publisher/model/Payment.java
+++ src/main/java/com/ikea/publisher/model/Payment.java
@@ -1,12 +1,16 @@
 package com.ikea.publisher.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import net.minidev.json.JSONObject;
 import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import java.math.BigInteger;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -20,13 +24,25 @@ public class Payment {
     @OneToOne(mappedBy = "player", cascade = CascadeType.ALL)
     public String firstName;
 
-    public BigInteger price;
+    public long price;
+
+    public String typeOfPrice;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    public String dateAndTime;
+
+    public String getDateAndTime(String dateAndTime) {
+        String.format(this.dateAndTime, "yyyy-MM-dd'T'HH:mm:ss");
+        return this.dateAndTime;
+    }
 
     public JSONObject toJson() {
         JSONObject json = new JSONObject();
         json.put("id", idPayments);
         json.put("firstName", firstName);
         json.put("price", price);
+        json.put("typeOfPrice", typeOfPrice);
+        json.put("dateAndTime", getDateAndTime(dateAndTime));
         return json;
     }
 }
